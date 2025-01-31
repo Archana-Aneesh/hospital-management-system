bb1aa9cdbfc24ec79fd303550cca5cec983c6e61
Archana <archanaaneesh7922@gmail.com>
2025-02-01 01:28:16 +0530

    fix

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose
-  .connect('mongodb://127.0.0.1:27017/hospitalDB')
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://<archanaaneesh>:<ArchuKukku7922>@cluster0.mongodb.net/hospitalDB?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
@@ -22,7 +26,7 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://127.0.0.1:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
  
 const Appointment = require('./models/appointment');
